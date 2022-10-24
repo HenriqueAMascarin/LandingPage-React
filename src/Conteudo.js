86d488ddadaec382a4a9ff946b8ff41a4827a5f7
LuckTheGamers <thegamers056@gmail.com>
2022-10-24 10:40:11 -0300

    first commit

---
+++ src/Conteudo.js
@@ -0,0 +1,26 @@
+function Conteudo(){
+    return(
+        <div className='conteudo'>
+        <div className='center'>
+          <div className='conteudo-single'>
+            <h3>Titulo da chamada</h3>
+            <p>Aqui é o meu conteúdo de exemplo para o site da danki code no Youtube! Espero que vc goste :)</p>
+          </div>
+          <div className='conteudo-single'>
+            <h3>Titulo da chamada</h3>
+            <p>Aqui é o meu conteúdo de exemplo para o site da danki code no Youtube! Espero que vc goste :)</p>
+          </div>
+          <div className='conteudo-single'>
+            <h3>Titulo da chamada</h3>
+            <p>Aqui é o meu conteúdo de exemplo para o site da danki code no Youtube! Espero que vc goste :)</p>
+          </div>
+          <div className='conteudo-single'>
+            <h3>Titulo da chamada</h3>
+            <p>Aqui é o meu conteúdo de exemplo para o site da danki code no Youtube! Espero que vc goste :)</p>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Conteudo;
